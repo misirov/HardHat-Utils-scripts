d914b816ca899e0744577f6d0946e9762092cda4
P. Misirov <47452703+misirov@users.noreply.github.com>
2022-03-16 19:35:10 +0100

    Update MinerOnOff.js

--- MinerOnOff.js
+++ MinerOnOff.js
@@ -1,3 +1,4 @@
+const { ethers } = require("hardhat");
 const hre = require("hardhat");
 
 /// Pass argument to script. Ex:
@@ -5,21 +6,19 @@ const hre = require("hardhat");
 ///     $ node ./MinerOnOff.js off
 
 const miner = async () => {
+    const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545")
     const myArgs = process.argv.slice(2);
-    if(myArgs[0] === 'off'){
-        console.log("Turning OFF automine...")
-        let a = await hre.network.provider.send("evm_setAutomine", [false]);
-        console.log(a, "... done")
-        return 0;
     
-    } else if(myArgs[0] === 'true') {
-        console.log("Turning on automine...")
-        await hre.network.provider.send("evm_setAutomine", [true]);
-        console.log("... done")
-        return 0;
-    } 
-    else{
-        console.log("please pass 'true' or 'false' as argument\n")
+    if(myArgs[0] === 'off'){
+        console.log("Turning OFF miner...\n")
+        await provider.send("evm_setIntervalMining", [0]);
+    
+    } else if(myArgs[0] === 'on') {
+        console.log('Turning ON, mining block every 5 seconds...')
+        await provider.send("evm_setIntervalMining", [5000]);
+
+    } else {
+        console.log("please pass 'true' OR 'false' OR 'interval' as an argument\n")
         return 1;
     }
 }
